811112679a735fe8a6508716381f202746ff13e0
David <davidiosub09@gmail.com>
2025-12-15 21:02:26 +0200

    flappy_bird

---
+++ CS50G/impl/dist/script.js
@@ -0,0 +1,7 @@
+"use strict";
+window.addEventListener("load", () => {
+    const canvas = document.getElementById("canvas1");
+    const ctx = canvas.getContext("2d");
+    const CANVAS_WIDTH = (canvas.width = window.innerWidth);
+    const CANVAS_HEIGHT = (canvas.height = window.innerHeight);
+});
